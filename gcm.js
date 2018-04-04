2236fa8ac9ed2868f5040144ced27e3941015840
Satit Rianpit <siteslave@msn.com>
2018-04-04 20:18:20 +0700

    add gcm

--- gcm.js
+++ gcm.js
@@ -1,21 +1,12 @@
 const gcm = require('node-gcm');
 const sender = new gcm.Sender('AAAAaD3EX0Q:APA91bHZ0nnhhkq8GsUG_-BiTnNBVRUxR6G_Fz4830zIQ557_Lkx-mrcB1ViPZkLfVIPuY3zjXvZFh3sO6fS9sQUJuZW4Au_Ddgn87_WwmrYyz87rBQb5Cf8wNObIeJa7h6anxX44bLh');
 
-let tokenDevice = "ckN66NQqOXE:APA91bFB5T6VZz89eG4Tqh0QRV9olcmjiJsZhovRbde4h-dBoAeELfwku858GKTpr58AHLYH57E_VD2-H4U9DF9ByqthHD3JcFeRPWPB5RkHa3AA_qj2Le6VdBgWj4uxDoklre3T2B-9"
+let tokenDevice = "cHPj2_HZ8Mk:APA91bHolB2RhQ1UNdQ1cKZelQDtK0-mUgCDjbxmpSGbilgETMJxA6k62tCsIWJqOYRyxgi3fMYdTuD0LMFJZcRQKwH6b3qKsos0ycvZ3-rks4mRn2tnwuOF-uPU-ArnGQ6eiaG8OK_z";
 var message = new gcm.Message({
-  priority: 'high',
   contentAvailable: true,
-  delayWhileIdle: true,
-  timeToLive: 3,
-  dryRun: true,
-  data: {
-    key1: 'message1',
-    key2: 'message2'
-  },
   notification: {
-    title: "Hello, World",
-    icon: "ic_launcher",
-    body: "This is a notification that will be displayed if your app is in the background."
+    title: "ทดสอบ Push notification",
+    body: "ทดสอบส่ง Message."
   }
 });
 
